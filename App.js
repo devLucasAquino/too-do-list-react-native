d32bd6bfdda5c44301a85f8c0a765e5588fc8cf9
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-07 20:46:11 -0300

    test user

--- App.js
+++ App.js
@@ -17,29 +17,6 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 16,
-  },
-  inputContainer : {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBorderColor: '#ccc',
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    width: '70%',
-    marginRight: 8,
-    padding: 8,
-  },
-  goalsContainer : {
-    flex: 5,
-  }
+ 
 
 });
