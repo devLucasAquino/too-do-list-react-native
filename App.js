d9101acf37bf95f1f2bd43b241d718310725dddb
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-07 21:32:59 -0300

    style: update style goalText

--- App.js
+++ App.js
@@ -36,7 +36,11 @@ export default function App() {
         />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+        {courseGoals.map((goal) => 
+          <View key={goal} style={styles.goalItem}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
+        )}
       </View>
     </View>
   );
@@ -66,6 +70,15 @@ const styles = StyleSheet.create({
   },
   goalsContainer : {
     flex: 5,
+  },
+  goalItem : {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+    },
+  goalText: {
+      color: 'white',    
   }
 
 });
