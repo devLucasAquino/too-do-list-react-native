2fee8240dc23768530b498332608e97347d15ec8
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-09 10:26:27 -0300

    working on the 'GoalInput' component

---
+++ components/GoalInput.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+import { View, TextInput, Button, StyleSheet } from "react-native";
+
+function GoalInput(props){
+
+    const [ enteredGoalText, setEnteredGoalText ] = useState('');
+
+    function goalInputHandler(enteredText){
+        setEnteredGoalText(enteredText);
+    };
+
+    function addGoalHandler(){
+        props.onAddGoal(enteredGoalText);
+        setEnteredGoalText('');
+    }
+    
+    return(
+        <View style={styles.inputContainer}>
+        <TextInput 
+          style={styles.textInput}
+          placeholder='Your course goal!'
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <Button 
+          title='Add Goal'
+          onPress={addGoalHandler}
+        />
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    inputContainer : {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 24,
+        borderBottomWidth: 1,
+        borderBorderColor: '#ccc',
+      },
+      textInput: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        width: '70%',
+        marginRight: 8,
+        padding: 8,
+      },
+})
+
+export default GoalInput;
