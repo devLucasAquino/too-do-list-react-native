d1cde239e9def8c0c1f594e44ab4b90a25a8f09a
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-09 11:08:07 -0300

    style: adding a modal screen

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,23 +3,31 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem( props ){
     return(
-        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-            <View style={styles.goalItem}>
+        <View style={styles.goalItem}>
+            <Pressable 
+                onPress={props.onDeleteItem.bind(this, props.id)}
+                //android_ripple={{color: '#210644'}}
+                style={({pressed}) => pressed && styles.pressedItem}
+            >
                 <Text style={styles.goalText}>{props.text}</Text>
-            </View>
-        </Pressable>
+            </Pressable>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
     goalItem : {
         margin: 8,
-        padding: 8,
         borderRadius: 6,
         backgroundColor: '#5e0acc',
-        },
+        }, 
+    pressedItem: {
+        backgroundColor: '#210644',
+        borderRadius: 6,
+    },
     goalText: {
-          color: 'white',    
+        padding: 8,
+        color: 'white',    
       }
 })
 
