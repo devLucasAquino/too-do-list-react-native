194f17faa76bae5e68f616689181ada32d2e6f6a
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-06-10 20:06:09 -0300

    app finishing touches and added StatusBar

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-
 import { StyleSheet, View, FlatList, Button } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
@@ -36,33 +36,36 @@ export default function App() {
 
 
   return (
-    <View style={styles.appContainer}>
-      <Button 
-        title='Add New Goal' 
-        color='#5e0acc' 
-        onPress={startAddGoalHandler}
-      />
-      <GoalInput 
-        onAddGoal={addGoalHandler}
-        onCancel={endAddGoalHandler}
-        visible={modalIsVisible}
-      />
-      <View  style={styles.goalsContainer}>
-        <FlatList data={courseGoals} renderItem={ (itemData) => {
-          return(
-            <GoalItem 
-              text={itemData.item.text} 
-              id={itemData.item.id}
-              onDeleteItem={deleteGoalHandler}
-            />
-          )
-        }} 
-        keyExtractor={(item, index) => {
-          return item.id;
-        }}
-        alwaysBounceVertical={false}/>
+    <>
+      <StatusBar style='light'/>
+      <View style={styles.appContainer}>
+        <Button 
+          title='Add New Goal' 
+          color='#a065ec' 
+          onPress={startAddGoalHandler}
+        />
+        <GoalInput 
+          onAddGoal={addGoalHandler}
+          onCancel={endAddGoalHandler}
+          visible={modalIsVisible}
+        />
+        <View  style={styles.goalsContainer}>
+          <FlatList data={courseGoals} renderItem={ (itemData) => {
+            return(
+              <GoalItem 
+                text={itemData.item.text} 
+                id={itemData.item.id}
+                onDeleteItem={deleteGoalHandler}
+              />
+            )
+          }} 
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          alwaysBounceVertical={false}/>
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
@@ -71,7 +74,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
-    backgroundColor: '#fff',
+
   },
   goalsContainer : {
     flex: 5,
